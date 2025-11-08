28f107ddaa107e4371875fe72b5bb2dc92fdbd2f
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2025-11-08 11:15:19 +0200

    Implement user api

--- front/src/components/PromptInput.tsx
+++ front/src/components/PromptInput.tsx
@@ -4,15 +4,17 @@ import React from "react";
 interface PromptInputProps {
   placeholder: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: any
 }
 
-export function PromptInput({ placeholder, onChange }: PromptInputProps) {
+export function PromptInput({ placeholder, onChange, value }: PromptInputProps) {
   return (
     <input
       type="text"
       className="prompt-input"
       placeholder={placeholder}
       onChange={onChange}
+      value={value}
     />
   );
 }
