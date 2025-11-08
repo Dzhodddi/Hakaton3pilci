38b696f5a60d356246d736a35a6595c674a78502
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2025-11-08 02:48:20 +0200

    Add protected routers

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,46 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import CreateProfilePage from './pages/CreateProfilePage';
+import {AuthProvider, useAuth} from './context/auth_context';
+import UserProfilePage from "./pages/UserProfilePage.tsx";
+import type {JSX} from "react";
+
+
+
+interface ProtectedRouteProps {
+    children: JSX.Element;
+}
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+    const { user } = useAuth();
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+    return children;
+}
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/newprofile' element={<CreateProfilePage />} />
-    </Routes>
-  );
+    <AuthProvider>
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/newprofile' element={<CreateProfilePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+                path='/profile'
+                element={
+                    <ProtectedRoute>
+                        <UserProfilePage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+    </AuthProvider>
+    );
 }
 
 export default App
